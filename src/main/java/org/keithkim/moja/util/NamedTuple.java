8a69dd6b3b2690a1867998d66a1ef159be967753
Keith Kim <karmakaze@users.noreply.github.com>
2021-10-20 09:45:28 -0400

    NamedTuple extends Map<String, Object>

--- src/main/java/org/keithkim/moja/util/NamedTuple.java
+++ src/main/java/org/keithkim/moja/util/NamedTuple.java
@@ -7,13 +7,15 @@ import java.util.*;
 
 import static java.util.Arrays.asList;
 
-public interface NamedTuple {
+public interface NamedTuple extends Map<String, Object> {
     String name();
-    Map<String, Object> namedValues();
+    List<String> names();
+    List<Object> values();
+    LinkedHashMap<String, Object> namedValues();
 
     static <S, A extends S, B extends S>
-    Map<String, S> namedValues(NamedPair<A, B> namedPair) {
-        Map<String, S> map = new LinkedHashMap<>(namedPair.width());
+    LinkedHashMap<String, S> namedValues(NamedPair<A, B> namedPair) {
+        LinkedHashMap<String, S> map = new LinkedHashMap<>(namedPair.width());
         for (int i = 0; i < namedPair.width(); i++) {
             map.put(namedPair.makeTuple.names.get(i), (S) namedPair.values[i]);
         }
@@ -21,8 +23,8 @@ public interface NamedTuple {
     }
 
     static <S, A extends S, B extends S, C extends S>
-    Map<String, S> namedValues(NamedTriple<A, B, C> namedTriple) {
-        Map<String, S> map = new LinkedHashMap<>(namedTriple.width());
+    LinkedHashMap<String, S> namedValues(NamedTriple<A, B, C> namedTriple) {
+        LinkedHashMap<String, S> map = new LinkedHashMap<>(namedTriple.width());
         for (int i = 0; i < namedTriple.width(); i++) {
             map.put(namedTriple.makeTuple.names.get(i), (S) namedTriple.values[i]);
         }
@@ -33,8 +35,8 @@ public interface NamedTuple {
         final String name;
         final List<String> names;
 
-        static Map<String, Object> namedObjectValues(List<String> names, Object[] values) {
-            Map<String, Object> map = new LinkedHashMap<>(values.length);
+        static LinkedHashMap<String, Object> namedObjectValues(List<String> names, Object[] values) {
+            LinkedHashMap<String, Object> map = new LinkedHashMap<>(values.length);
             for (int i = 0; i < values.length; i++) {
                 map.put(names.get(i), values[i]);
             }
@@ -42,7 +44,7 @@ public interface NamedTuple {
         }
 
         static String toString(NamedTuple namedTuple) {
-            Map<String, Object> map = namedTuple.namedValues();
+            LinkedHashMap<String, Object> map = namedTuple.namedValues();
             StringBuilder buffer = new StringBuilder(namedTuple.name() + "(");
             for (Map.Entry<String, ?> me : map.entrySet()) {
                 buffer.append(me.getKey());
@@ -100,7 +102,17 @@ public interface NamedTuple {
         }
 
         @Override
-        public Map<String, Object> namedValues() {
+        public List<String> names() {
+            return makeTuple.names;
+        }
+
+        @Override
+        public List<Object> values() {
+            return asList(values);
+        }
+
+        @Override
+        public LinkedHashMap<String, Object> namedValues() {
             return MakeNamedTuple.namedObjectValues(makeTuple.names, values);
         }
 
@@ -108,6 +120,7 @@ public interface NamedTuple {
         public String toString() {
             return MakeNamedTuple.toString(this);
         }
+
     }
 
     class NamedTriple<A, B, C> extends Triple<A, B, C> implements NamedTuple {
@@ -124,7 +137,17 @@ public interface NamedTuple {
         }
 
         @Override
-        public Map<String, Object> namedValues() {
+        public List<String> names() {
+            return makeTuple.names;
+        }
+
+        @Override
+        public List<Object> values() {
+            return asList(values);
+        }
+
+        @Override
+        public LinkedHashMap<String, Object> namedValues() {
             return MakeNamedTuple.namedObjectValues(makeTuple.names, values);
         }
 
@@ -132,5 +155,86 @@ public interface NamedTuple {
         public String toString() {
             return MakeNamedTuple.toString(this);
         }
+    }
+
+    default Object get(Object key) {
+        int i = names().indexOf(key);
+        return i < 0 ? null : values().get(i);
+    }
+
+    default String getString(String name) {
+        return (String) get(name);
+    }
+    default Boolean getBoolean(String name) {
+        return (Boolean) get(name);
+    }
+    default Integer getInteger(String name) {
+        return (Integer) get(name);
+    }
+    default Long getLong(String name) {
+        return (Long) get(name);
+    }
+    default Float getFloat(String name) {
+        return (Float) get(name);
+    }
+    default Double getDouble(String name) {
+        return (Double) get(name);
+    }
+    default <V> List<V> getList(String name) {
+        return (List<V>) get(name);
+    }
+    default <V> Set<V> getSet(String name) {
+        return (Set<V>) get(name);
+    }
+    default <K, V> Map<K, V> getMap(String name) {
+        return (Map<K, V>) get(name);
+    }
+
+    default Object put(String key, Object value) {
+        int i = names().indexOf(key);
+        if (i < 0) {
+            throw new UnsupportedOperationException();
+        }
+        Object oldValue = values().get(i);
+        values().set(i, value);
+        return oldValue;
+    }
+
+    default void putAll(Map<? extends String, ?> m) {
+        for (Map.Entry<? extends String, ?> me : m.entrySet()) {
+            put(me.getKey(), me.getValue());
+        }
+    }
+
+    default int size() {
+        return names().size();
+    }
+
+    default boolean isEmpty() {
+        return size() == 0;
+    }
+
+    default boolean containsKey(Object key) {
+        return names().contains(key);
+    }
+
+    default boolean containsValue(Object value) {
+        return values().contains(value);
+    }
+
+    default Object remove(Object key) {
+        throw new UnsupportedOperationException();
+    }
+
+    default void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    default Set<String> keySet() {
+        return new HashSet<>(names());
+    }
+
+    default Set<Entry<String, Object>> entrySet() {
+        return namedValues().entrySet();
     }
 }
